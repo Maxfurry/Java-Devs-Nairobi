502f552fb7a07979e50d4e6a74aed5e6bd71e034
Maxfurry <adeoyemark@gmail.com>
2019-03-26 12:33:51 +0100

    [#164771583] Connect recycler view layout to MainActivity

--- app/src/main/java/com/example/javadevnai/MainActivity.java
+++ app/src/main/java/com/example/javadevnai/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.javadevnai;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
     }
 }
