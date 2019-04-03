aeedeae7bafec3218e0cfce9e925316dd39a3111
Maxfurry <adeoyemark@gmail.com>
2019-04-03 17:05:17 +0100

    [#164771606] pass data from recycler view to detail view

--- app/src/main/java/com/example/javadevnai/view/DevDetails.java
+++ app/src/main/java/com/example/javadevnai/view/DevDetails.java
@@ -4,14 +4,33 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.javadevnai.R;
+import com.example.javadevnai.model.JavaGithubNai;
+import com.example.javadevnai.presenter.GithubPresenter;
+import com.squareup.picasso.Picasso;
 
-public class DevDetails extends AppCompatActivity {
+public class DevDetails extends AppCompatActivity implements JavaGithubUserView {
+
     Context context = this;
+
+    ImageView avatar;
+    TextView username;
+    TextView fullname;
+    TextView githubUrl;
+    TextView bio;
+    TextView followers;
+    TextView followings;
+    TextView repo;
+    TextView organisation;
+
+    GithubPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,21 @@ public class DevDetails extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        avatar = this.findViewById(R.id.display_pic);
+        username = this.findViewById(R.id.username);
+        fullname = this.findViewById(R.id.full_name);
+        githubUrl = this.findViewById(R.id.githubUrl);
+        bio = this.findViewById(R.id.bio);
+        followers = this.findViewById(R.id.followers);
+        followings = this.findViewById(R.id.following);
+        repo = this.findViewById(R.id.repositories);
+        organisation = this.findViewById(R.id.organisation);
+
+        presenter = new GithubPresenter(this);
+        String username = getIntent().getExtras().getString("username");
+        presenter.getJavaUser(username);
+        Log.d("Name", username);
     }
 
     @Override
@@ -39,4 +73,30 @@ public class DevDetails extends AppCompatActivity {
 
     }
 
+    @Override
+    public void displayJavaUser(JavaGithubNai javaGithubNaiUser) {
+        Picasso.get().load(javaGithubNaiUser.getAvatarUrl()).into(avatar);
+        username.setText(javaGithubNaiUser.getUsername());
+        fullname.setText(javaGithubNaiUser.getName());
+
+        followers.setText(javaGithubNaiUser.getFollowers()+"");
+        followings.setText(javaGithubNaiUser.getFollowing()+"");
+        repo.setText(javaGithubNaiUser.getRepo()+"");
+        githubUrl.setText(javaGithubNaiUser.getHtmlUrl());
+
+        String info = javaGithubNaiUser.getBio();
+        String company = javaGithubNaiUser.getCompany();
+
+        if (company != null) {
+            organisation.setText("\u2605 " + company);
+        } else {
+            organisation.setText("N/A");
+        }
+
+        if (company != null) {
+            bio.setText(info);
+        } else {
+            bio.setText("N/A");
+        }
+    }
 }
